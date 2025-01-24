7d6f0c76e78c3a1029438cfa63e54830ec205671
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2025-01-24 17:31:37 +0000

    wip added matomo with url change inserted dot before netifly to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
     var _mtm = window._mtm = window._mtm || [];
     _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://cdn.matomo.cloud/bandswebsitenetlifyapp.matomo.cloud/container_LOfPXoAa.js'; s.parentNode.insertBefore(g,s);
+    g.async=true; g.src='https://cdn.matomo.cloud/bandswebsite.netlifyapp.matomo.cloud/container_LOfPXoAa.js'; s.parentNode.insertBefore(g,s);
    }, [])
  
   return (
