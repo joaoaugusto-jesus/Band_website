19c029f0cfdd2a0375ac54f63f971b95cef33d48
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2025-01-24 17:26:39 +0000

    wip added matomo to layout.js

--- src/app/components/Footer/page.js
+++ src/app/components/Footer/page.js
@@ -2,7 +2,12 @@ import Image from "next/image";
 import styles from "./Footer.module.css";
 import Link from "next/link";
 
+
 export default function Footer() {
+
+     
+
+
     return (
         <div className={styles.container}>
         <div className={styles.content}>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import React, { useEffect } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,7 +18,17 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+    useEffect(() => {
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/bandswebsitenetlifyapp.matomo.cloud/container_LOfPXoAa.js'; s.parentNode.insertBefore(g,s);
+   }, [])
+ 
   return (
+
+    
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
