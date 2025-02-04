f5416937152fa87201ebabb70757bf15d853f4bf
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2025-02-04 23:42:13 +0000

    wip fixing configuration of next and auth0

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Conditionally add output based on environment
-    ...(process.env.NODE_ENV === 'production' ? { output: 'export' } : {}),
+  images: {
+    domains: ['res.cloudinary.com'], // Allow external images
+    unoptimized: true, // Ensures images work without Next.js optimization in static export
+  },
+};
 
-    images: {
-      domains: ['res.cloudinary.com'], // Allow external images
-      unoptimized: true, // Ensures images work without Next.js optimization in static export
-    },
-  };
-  
-  export default nextConfig;
+export default nextConfig;
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,29 +1,17 @@
+export const dynamic = 'force-dynamic';
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-
-
 
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOptions);
+const handler = (req, res) => NextAuth(req, res, authOptions);
 
-// ✅ Named exports for Next.js App Router
 export { handler as GET, handler as POST };
