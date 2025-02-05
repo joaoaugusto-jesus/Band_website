4911e342fdf0c93d2e35b433fade6b517065c1c7
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2025-02-05 13:01:54 +0000

    updating netifly, installed netifly plugin, added netifly.toml and changes on next.config.jms

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'], // Allow external images
-    unoptimized: true, // Ensures images work without Next.js optimization in static export
+    domains: ['res.cloudinary.com'], // Keep your Cloudinary settings
   },
+  output: 'standalone', // Enables standalone build for SSR support
 };
 
 export default nextConfig;
+
