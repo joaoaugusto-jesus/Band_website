116a236e27398200683660e333c73b34d9b34bc1
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2025-02-10 17:30:53 +0000

    Fix authentication issue with Google and CredentialsProvider

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,7 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
