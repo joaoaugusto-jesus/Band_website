f98d38db4c22432091d66e047be66feb74772091
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2025-02-18 15:25:57 +0000

    wip push to deploy

--- src/app/components/Merchandise/page.js
+++ src/app/components/Merchandise/page.js
@@ -18,6 +18,7 @@ export default function Merchandise() {
               src={shirt.src}
               name={shirt.name}
               price={shirt.price}
+              isShirt={true}
                   />
          
         ))
