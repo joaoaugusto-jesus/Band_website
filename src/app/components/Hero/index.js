b65be62385092ea2700cd9d8243deb4d1e1f0690
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2025-04-30 17:41:54 +0100

    wip adding aimations with frame-motion in hero and Merchandise components

--- src/app/components/Hero/index.js
+++ src/app/components/Hero/index.js
@@ -11,37 +11,66 @@ import Agenda from "../Agenda/page";
 import Merchandise from "../Merchandise";
 import Image from "next/image";
 import { videos } from "../../Data/music-videos";
-import { motion } from "framer-motion";
+import {  motion, useAnimation, useInView, whileHover } from "framer-motion";
+import { useEffect } from "react";
+
 
 export default function Hero() {
-  
+
+
+
+const controls = useAnimation();
+const headerRef = useRef(null);
+const isInView = useInView(headerRef, { once: false, margin: "-50px" });
+const whileHover = useInView(headerRef, { once: false, margin: "-50px" });
+
+
+useEffect(() => {
+  if (isInView) {
+    controls.start({
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.8, ease: "easeInOut" },
+    });
+  } else {
+    controls.start({ opacity: 0, x: -100 });
+  }
+}, [isInView, controls]);
+
+
   return (
+  
     <div className={styles.layout}>
-      <motion.div
-       initial={{ opacity: 0, y: 50 }}
-       animate={{ opacity: 1, y: 0 }}
-       transition={{ duration: 0.8 }}
-        >
+      
+       
+        
       <section className={styles.sectionHeader}>
       
         
  
      </section>
-     </motion.div>
+    
      <section className={styles.sectionContent}>
       <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
+         ref={headerRef}
+         animate={controls}
+         initial={{ opacity: 0, x: 100 }}
+         
         className={styles.headerTitle}>
      
           <h1 className={styles.title}>Your Sports Partner</h1>
-          <p className={styles.description}>The last rock album made by humans</p>
+          <p className={styles.description}>The last rock album </p>
+          <p className={styles.description}>made by humans</p>
           <a href="/store"><h2 className={styles.subtitle}>Buy now and get free Cartel shipping</h2></a>
         
         </motion.div>
        <div className={styles.videoContainer}>
-         
+         <motion.div
+           initial={{ opacity: 0, x: 100 }}
+           animate={{ opacity: 1, x: 0 }}
+           transition={{ duration: 0.8, ease: "easeInOut" }}
+           whileHover={{ scale: 1.20 }}
+           className={styles.videoBg}>
           <video 
               className={styles.videoTeaser}  
               controls
@@ -50,6 +79,7 @@ export default function Hero() {
             <source src={videos[0].src} type="video/mp4" />
             Your browser does not support the video element.
           </video>
+          </motion.div>
         </div>
      </section>
       <Merchandise />
--- src/app/components/Merchandise/index.js
+++ src/app/components/Merchandise/index.js
@@ -1,20 +1,44 @@
 'use client';
 import { shirts } from '../../Data/shirts';
 import styles from './Merchandise.module.css';
-
+import {  motion, useAnimation, useInView  } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { useState } from 'react';
 import Card from '../Card/page';
 
 
 export default function Merchandise() {
 
+  const controls = useAnimation();
+  const headerRef = useRef(null);
+  const isInView = useInView(headerRef, { once: false, margin: "-100px" });
+  
+  useEffect(() => {
+    if (isInView) {
+      controls.start({
+        opacity: 1,
+        x: 0,
+        transition: { duration: 0.8, ease: "easeInOut" },
+      });
+    } else {
+      controls.start({ opacity: 0, x: 100 });
+    }
+  }, [isInView, controls]);
 
   return (
     <div className={styles.container}>
+      <motion.div
+          ref={headerRef}
+          animate={controls}
+          initial={{ opacity: 0, x: 100 }}
+          className={styles.header}
+         >
          <h1
           className={styles.title}
       >
         Merchandise
       </h1>
+    
       <div className={styles.grid}>
         {
         shirts && shirts.length > 0 ? 
@@ -34,7 +58,7 @@ export default function Merchandise() {
         }  
        
       </div>
-    
+      </motion.div>
     </div>
   );
 }
